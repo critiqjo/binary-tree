47e9167a44daf2fcfb003791e504e92948b6cd1b
critiqjo <john.ch.fr@gmail.com>
2016-04-24 21:31:48 +0530

    Keep it simple: CountTree

--- src/count.rs
+++ src/count.rs
@@ -1,3 +1,11 @@
+//! Counting trees.
+//!
+//! ## When should you use CountTree?
+//!
+//! - You want to maintain a possibly large unsorted list.
+//! - You want to access, modify, insert, and delete elements at arbitrary
+//!   position without incurring O(n) time complexity.
+
 use std::mem;
 
 use Node;
@@ -5,25 +13,21 @@ use NodeMut;
 use BinaryTree;
 use iter::NodeIter;
 
-pub trait Countable {
-    fn count(&self) -> u64;
-}
-
-impl Countable for u64 {
-    fn count(&self) -> u64 {
-        *self
-    }
-}
+pub type NodePtr<T> = Box<CountNode<T>>;
 
 pub struct CountTree<T>(CountNode<T>);
 
-impl<T: Countable> CountTree<T> {
+impl<T> CountTree<T> {
     pub fn new(val: T) -> CountTree<T> {
         CountTree(CountNode::new(val))
     }
+
+    // TODO get, get_mut, insert, delete, len, {push|pop}_{front|back}
+    // TODO [?] clear, is_empty, iter_mut
+    // TODO [hard] truncate, append, split_off, impl FromIterator, retain
 }
 
-impl<T: Countable> BinaryTree for CountTree<T> {
+impl<T> BinaryTree for CountTree<T> {
     type Node = CountNode<T>;
 
     fn root(&self) -> &Self::Node {
@@ -31,9 +35,7 @@ impl<T: Countable> BinaryTree for CountTree<T> {
     }
 }
 
-impl<'a, T> IntoIterator for &'a CountTree<T>
-    where T: Countable
-{
+impl<'a, T> IntoIterator for &'a CountTree<T> {
     type Item = &'a T;
     type IntoIter = NodeIter<'a, CountNode<T>>;
 
@@ -44,29 +46,29 @@ impl<'a, T> IntoIterator for &'a CountTree<T>
 
 pub struct CountNode<T> {
     val: T,
-    left: Option<Box<CountNode<T>>>,
-    right: Option<Box<CountNode<T>>>,
-    left_sum: u64, // excluding val.count()
-    right_sum: u64, // ditto
+    left: Option<NodePtr<T>>,
+    right: Option<NodePtr<T>>,
+    lcount: u64, // not counting oneself
+    rcount: u64, // ditto
 }
 
-impl<T: Countable> CountNode<T> {
+impl<T> CountNode<T> {
     pub fn new(val: T) -> CountNode<T> {
         CountNode {
             val: val,
             left: None,
             right: None,
-            left_sum: 0,
-            right_sum: 0,
+            lcount: 0,
+            rcount: 0,
         }
     }
 
-    pub fn total_count(&self) -> u64 {
-        self.left_sum + self.val.count() + self.right_sum
+    pub fn count(&self) -> u64 {
+        self.lcount + 1 + self.rcount
     }
 }
 
-impl<T: Countable> Node for CountNode<T> {
+impl<T> Node for CountNode<T> {
     type Value = T;
 
     fn left(&self) -> Option<&Self> {
@@ -82,27 +84,27 @@ impl<T: Countable> Node for CountNode<T> {
     }
 }
 
-impl<T: Countable> NodeMut for CountNode<T> {
-    type NodePtr = Box<CountNode<T>>;
+impl<T> NodeMut for CountNode<T> {
+    type NodePtr = NodePtr<T>;
 
     fn detach_left(&mut self) -> Option<Self::NodePtr> {
-        self.left_sum = 0;
+        self.lcount = 0;
         self.left.take()
     }
 
     fn detach_right(&mut self) -> Option<Self::NodePtr> {
-        self.right_sum = 0;
+        self.rcount = 0;
         self.right.take()
     }
 
     fn insert_left(&mut self, mut tree: Option<Self::NodePtr>) -> Option<Self::NodePtr> {
-        self.left_sum = tree.as_ref().map_or(0, |tree| tree.total_count());
+        self.lcount = tree.as_ref().map_or(0, |tree| tree.count());
         mem::swap(&mut self.left, &mut tree);
         tree
     }
 
     fn insert_right(&mut self, mut tree: Option<Self::NodePtr>) -> Option<Self::NodePtr> {
-        self.right_sum = tree.as_ref().map_or(0, |tree| tree.total_count());
+        self.rcount = tree.as_ref().map_or(0, |tree| tree.count());
         mem::swap(&mut self.right, &mut tree);
         tree
     }
@@ -124,8 +126,8 @@ mod tests {
         ct_l.insert_right(Some(Box::new(CountNode::new(12))));
         ct.insert_left(Some(Box::new(ct_l)));
         ct.insert_right(Some(Box::new(CountNode::new(5))));
-        assert_eq!(ct.left_sum, 20);
-        assert_eq!(ct.right_sum, 5);
-        assert_eq!(ct.total_count(), 32);
+        assert_eq!(ct.lcount, 2);
+        assert_eq!(ct.rcount, 1);
+        assert_eq!(ct.count(), 4);
     }
 }

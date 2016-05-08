7f054780f65afa3d1ea46a423b9851d4de254d43
critiqjo <john.ch.fr@gmail.com>
2016-05-08 13:42:46 +0530

    Better randomized benchmarks

--- benches/rand_insert.rs
+++ benches/rand_insert.rs
@@ -5,29 +5,48 @@ extern crate test;
 extern crate binary_tree;
 
 use binary_tree::count::CountTree;
-use rand::{Rng, StdRng};
+use rand::{Rand, Rng, StdRng, SeedableRng, thread_rng};
+use std::env;
+use std::str::FromStr;
 use test::Bencher;
+
+const TOTAL: usize = 7936;
+
+fn get_seed() -> usize {
+    match env::var("INSERT_SEED") {
+        Ok(val) => usize::from_str(&*val).unwrap(),
+        Err(_) => {
+            let seed = usize::rand(&mut thread_rng());
+            env::set_var("INSERT_SEED", seed.to_string());
+            seed
+        }
+    }
+}
 
 #[bench]
 pub fn insert_at_random_ct(b: &mut Bencher) {
-    let mut rng = StdRng::new().unwrap();
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
     b.iter(|| {
+        let mut rng = rng.clone();
         let mut ct = CountTree::new();
         ct.insert(0, 0);
-        for i in 1..7936 {
+        for i in 1..TOTAL {
             ct.insert(rng.gen_range(0, i), i);
         }
-    })
+    });
 }
 
 #[bench]
 pub fn insert_at_random_vec(b: &mut Bencher) {
-    let mut rng = StdRng::new().unwrap();
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
     b.iter(|| {
+        let mut rng = rng.clone();
         let mut v = Vec::new();
         v.insert(0, 0);
-        for i in 1..7936 {
+        for i in 1..TOTAL {
             v.insert(rng.gen_range(0, i), i);
         }
-    })
+    });
 }

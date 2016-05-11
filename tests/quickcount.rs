fca3be7095541fd689fe4d0955c667bbf13e4b6c
critiqjo <john.ch.fr@gmail.com>
2016-05-11 19:16:24 +0530

    Quickcheck: +qc_remove, +qc_assert!

--- tests/quickcount.rs
+++ tests/quickcount.rs
@@ -10,8 +10,17 @@ use binary_tree::count::CountTree;
 use binary_tree::test::compute_level;
 use quickcheck::TestResult;
 
+macro_rules! qc_assert {
+    ($cond:expr) => {
+        if !($cond) {
+            return TestResult::failed();
+        }
+    }
+}
+
+
 #[quickcheck]
-fn ct_insert(mut ct: CountTree<usize>, index: usize) -> TestResult {
+fn qc_insert(mut ct: CountTree<usize>, index: usize) -> TestResult {
     if index > ct.len() {
         return TestResult::discard();
     }
@@ -25,14 +34,43 @@ fn ct_insert(mut ct: CountTree<usize>, index: usize) -> TestResult {
     let next = ct.get(index).map(|v| *v);
 
     ct.insert(index, this);
-    let mut ok = true;
     if let Some(prev) = prev {
-        ok = ok && prev == *ct.get(index - 1).unwrap();
+        qc_assert!(prev == *ct.get(index - 1).unwrap());
     }
-    ok = ok && this == *ct.get(index).unwrap();
+    qc_assert!(this == *ct.get(index).unwrap());
     if let Some(next) = next {
-        ok = ok && next == *ct.get(index + 1).unwrap();
+        qc_assert!(next == *ct.get(index + 1).unwrap());
     }
-    ok = ok && compute_level(ct.root().unwrap(), 1).is_balanced();
-    TestResult::from_bool(ok)
+    let level = compute_level(ct.root().unwrap(), 1);
+    qc_assert!(level.is_balanced());
+
+    TestResult::passed()
+}
+
+#[quickcheck]
+fn qc_remove(mut ct: CountTree<usize>, index: usize) -> TestResult {
+    if index + 1 > ct.len() {
+        return TestResult::discard();
+    }
+
+    let prev = if index > 0 {
+        ct.get(index - 1).map(|v| *v)
+    } else {
+        None
+    };
+    let next = ct.get(index + 1).map(|v| *v);
+
+    ct.remove(index);
+    if let Some(prev) = prev {
+        qc_assert!(prev == *ct.get(index - 1).unwrap());
+    }
+    if let Some(next) = next {
+        qc_assert!(next == *ct.get(index).unwrap());
+    }
+    if let Some(root) = ct.root() {
+        let level = compute_level(root, 1);
+        qc_assert!(level.is_balanced());
+    }
+
+    TestResult::passed()
 }

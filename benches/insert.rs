5d163c22a81cfadf2b3f5098af8143a38c977d4e
critiqjo <john.ch.fr@gmail.com>
2016-05-12 13:38:12 +0530

    Benches: replace *_at_k_* with randomized

--- benches/insert.rs
+++ benches/insert.rs
@@ -1,41 +1,67 @@
 #![feature(test)]
 
-extern crate test;
 extern crate binary_tree;
+extern crate rand;
+extern crate test;
 
 use binary_tree::count::CountTree;
+use rand::{Rand, Rng, StdRng, SeedableRng, thread_rng};
+use std::env;
+use std::str::FromStr;
+use std::collections::LinkedList;
 use test::Bencher;
 
-use std::collections::LinkedList;
+const TOTAL: usize = 4096;
 
-#[bench]
-pub fn insert_at_k_ct(b: &mut Bencher) {
-    b.iter(|| {
-        let mut ct: CountTree<_> = (0..4000).collect();
-        for i in 0..4000 {
-            ct.insert(300, i);
+fn get_seed() -> usize {
+    match env::var("RAND_SEED") {
+        Ok(val) => usize::from_str(&*val).unwrap(),
+        Err(_) => {
+            let seed = usize::rand(&mut thread_rng());
+            env::set_var("RAND_SEED", seed.to_string());
+            seed
         }
-    })
+    }
 }
 
 #[bench]
-pub fn insert_at_k_ll(b: &mut Bencher) {
+pub fn insert_at_random_ct(b: &mut Bencher) {
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
     b.iter(|| {
-        let mut ll: LinkedList<_> = (0..4000).collect();
-        for i in 0..4000 {
-            let mut tail = ll.split_off(300);
+        let mut rng = rng.clone();
+        let mut ct = CountTree::new();
+        ct.insert(0, 0); // magical speed-up
+        for i in 1..TOTAL { // starts at 1, not 0
+            ct.insert(rng.gen_range(0, i + 1), i);
+        }
+    });
+}
+
+#[bench]
+pub fn insert_at_random_ll(b: &mut Bencher) {
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
+    b.iter(|| {
+        let mut rng = rng.clone();
+        let mut ll = LinkedList::new();
+        for i in 0..TOTAL {
+            let mut tail = ll.split_off(rng.gen_range(0, i + 1));
             ll.push_back(i);
             ll.append(&mut tail);
         }
-    })
+    });
 }
 
 #[bench]
-pub fn insert_at_k_vec(b: &mut Bencher) {
+pub fn insert_at_random_vec(b: &mut Bencher) {
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
     b.iter(|| {
-        let mut v: Vec<_> = (0..4000).collect();
-        for i in 0..4000 {
-            v.insert(300, i);
+        let mut rng = rng.clone();
+        let mut v = Vec::new();
+        for i in 0..TOTAL {
+            v.insert(rng.gen_range(0, i + 1), i);
         }
-    })
+    });
 }
--- benches/remove.rs
+++ benches/remove.rs
@@ -1,41 +1,66 @@
 #![feature(test)]
 
-extern crate test;
 extern crate binary_tree;
+extern crate rand;
+extern crate test;
 
 use binary_tree::count::CountTree;
+use rand::{Rand, Rng, StdRng, SeedableRng, thread_rng};
+use std::env;
+use std::str::FromStr;
+use std::collections::LinkedList;
 use test::Bencher;
 
-use std::collections::LinkedList;
+const TOTAL: usize = 4096;
 
-#[bench]
-pub fn remove_at_k_ct(b: &mut Bencher) {
-    b.iter(|| {
-        let mut ct: CountTree<_> = (0..6000).collect();
-        for _ in 0..3000 {
-            ct.remove(300);
+fn get_seed() -> usize {
+    match env::var("RAND_SEED") {
+        Ok(val) => usize::from_str(&*val).unwrap(),
+        Err(_) => {
+            let seed = usize::rand(&mut thread_rng());
+            env::set_var("RAND_SEED", seed.to_string());
+            seed
         }
-    })
+    }
 }
 
 #[bench]
-pub fn remove_at_k_ll(b: &mut Bencher) {
+pub fn remove_at_random_ct(b: &mut Bencher) {
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
     b.iter(|| {
-        let mut ll: LinkedList<_> = (0..6000).collect();
-        for _ in 0..3000 {
-            let mut tail = ll.split_off(300);
-            ll.pop_back();
+        let mut rng = rng.clone();
+        let mut ct: CountTree<_> = (0..TOTAL).collect();
+        for i in 0..TOTAL {
+            ct.remove(rng.gen_range(0, TOTAL - i));
+        }
+    });
+}
+
+#[bench]
+pub fn remove_at_random_ll(b: &mut Bencher) {
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
+    b.iter(|| {
+        let mut rng = rng.clone();
+        let mut ll: LinkedList<_> = (0..TOTAL).collect();
+        for i in 0..TOTAL {
+            let mut tail = ll.split_off(rng.gen_range(0, TOTAL - i));
+            tail.pop_front();
             ll.append(&mut tail);
         }
-    })
+    });
 }
 
 #[bench]
-pub fn remove_at_k_vec(b: &mut Bencher) {
+pub fn remove_at_random_vec(b: &mut Bencher) {
+    let seed = get_seed();
+    let rng = StdRng::from_seed(&[seed]);
     b.iter(|| {
-        let mut v: Vec<_> = (0..6000).collect();
-        for _ in 0..3000 {
-            v.remove(300);
+        let mut rng = rng.clone();
+        let mut v: Vec<_> = (0..TOTAL).collect();
+        for i in 0..TOTAL {
+            v.remove(rng.gen_range(0, TOTAL - i));
         }
-    })
+    });
 }
